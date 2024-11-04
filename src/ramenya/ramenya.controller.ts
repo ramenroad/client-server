8e867caa94fa73937d05eaecdc4d5f6091107662
kimjunseoo <zz0905k@naver.com>
2024-11-04 12:04:04 +0900

    feat: 라멘야 생성하기

--- src/ramenya/ramenya.controller.ts
+++ src/ramenya/ramenya.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { RamenyaService } from './ramenya.service';
+import { createRamenyaReqDTO } from './dto/req/createRamenya.req.dto';
 
 @Controller('ramenya')
 export class RamenyaController {
-    constructor(private readonly ramenyaService: RamenyaService){}
+  constructor(private readonly ramenyaService: RamenyaService) {}
 
-    @Get('')
-    getRamenyas(){
-        
-    }
+  @Get('')
+  getRamenyas() {}
+
+  @Post('')
+  createRamenya(@Body() dto: createRamenyaReqDTO): Promise<void> {
+    return this.ramenyaService.createRamenya(dto);
+  }
 }
