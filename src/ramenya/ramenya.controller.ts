ef85159304b45f9f76be2602c70cbf6f4a787525
kimjunseoo <zz0905k@naver.com>
2025-01-08 03:33:20 +0900

    feat: 라멘야 전체 검색 - 장르별조회

--- src/ramenya/ramenya.controller.ts
+++ src/ramenya/ramenya.controller.ts
@@ -10,12 +10,31 @@ export class RamenyaController {
 
   @ApiOperation({
     summary: '라멘야 전체 정보 조회',
-    description: '저장된 모든 라멘야 리스트를 조회합니다.',
+    description:
+      '저장된 모든 라멘야 리스트를 조회합니다. 지역별/장르별 기준을 쿼리로 선택할 수 있습니다',
   })
   @ApiQuery({
     name: 'region',
     required: false,
     type: String,
+  })
+  @ApiQuery({
+    name: 'genre',
+    required: false,
+    type: String,
+    description: '라멘 장르(ex. 돈코츠, 시오)',
+    examples: {
+      active: {
+        summary: '검색하고자 하는 장르가 1가지',
+        description: '검색하고자 하는 장르가 1가지일 경우 단어 하나',
+        value: '시오',
+      },
+      inactive: {
+        summary: '검색하고자 하는 장르가 2가지 이상',
+        description: '장르가 2가지 이상일 경우 쉼표(,)로 구분',
+        value: '시오,쇼유',
+      },
+    },
   })
   @ApiResponse({
     status: 200,
@@ -24,8 +43,11 @@ export class RamenyaController {
     isArray: true,
   })
   @Get('/all')
-  getRamenyas(@Query('region') region?: string): Promise<getRamenyasResDTO[]> {
-    return this.ramenyaService.getRamenyas(region);
+  getRamenyas(
+    @Query('region') region?: string,
+    @Query('genre') genre?: string,
+  ): Promise<getRamenyasResDTO[]> {
+    return this.ramenyaService.getRamenyas(region, genre);
   }
 
   @ApiOperation({
