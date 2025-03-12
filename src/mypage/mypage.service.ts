e48de9c9ec4bacc5ddcc2a16d480c6d6f0a8b536
kimjunseoo <zz0905k@naver.com>
2025-03-12 19:39:40 +0900

    fix: import

--- src/mypage/mypage.service.ts
+++ src/mypage/mypage.service.ts
@@ -3,6 +3,7 @@ import { updateNicknameReqDTO } from './dto/req/updateNickname.req.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { user } from 'schema/user.schema';
+import { JwtPayload } from 'src/common/types/jwtpayloadtype';
 
 @Injectable()
 export class MypageService {
