4798a3318b92af2523e269a97db59e2143f2d83b
kimjunseoo <zz0905k@naver.com>
2025-03-12 13:00:21 +0900

    Fix: kakao social login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { signUpUserByKakaoReqDTO } from './dto/req/signUpUserByKakao.req.dto';
 import { signInUserByKakakoReqDTO } from './dto/req/signInUserByKakao.req.dto';
 import { Public } from 'src/common/decorators/public.decorator';
+import { Response } from 'express';
+import { signInUserByKakakoResDTO } from './dto/res/signInUserByKakao.res.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -12,43 +14,28 @@ export class AuthController {
   @Public()
   @ApiOperation({
     summary: '카카오 소셜 로그인',
+    description:
+      '회원가입이 되어있지 않은 유저 정보는 자동 회원가입 후 토큰이 발급됩니다.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '기존 회원 로그인 성공',
+    type: signInUserByKakakoResDTO,
   })
   @ApiResponse({
     status: 201,
-    description: '로그인 성공',
-  })
-  @ApiResponse({
-    status: 301,
-    description:
-      '해당 토큰 정보로 가입된 회원 정보가 없는 경우 // 회원가입 필요',
+    description: '회원가입 및 로그인 성공',
+    type: signInUserByKakakoResDTO,
   })
   @ApiResponse({
     status: 500,
     description: '사용자 정보 불러오기에 실패한 경우',
   })
   @Post('/signin/kakao')
-  signInUserByKakao(@Body() dto: signInUserByKakakoReqDTO) {
-    return this.authService.signInUserByKakao(dto);
-  }
-
-  @Public()
-  @ApiOperation({
-    summary: '카카오 소셜 회원가입',
-  })
-  @ApiResponse({
-    status: 201,
-    description: '카카오 소셜 회원가입 성공',
-  })
-  @ApiResponse({
-    status: 406,
-    description: '해당 카카오 계정으로 이미 회원가입이 되어있는 경우',
-  })
-  @ApiResponse({
-    status: 500,
-    description: '카카오 서버에서 토큰을 불러오지 못한 경우',
-  })
-  @Post('/signup/kakao')
-  signUpUserByKakao(@Body() dto: signUpUserByKakaoReqDTO): Promise<void> {
-    return this.authService.signUpUserByKakao(dto);
+  signInUserByKakao(
+    @Body() dto: signInUserByKakakoReqDTO,
+    @Res() res: Response,
+  ) {
+    return this.authService.signInUserByKakao(dto, res);
   }
 }
