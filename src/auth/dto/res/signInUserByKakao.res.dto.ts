fea8efe2afc307cc1417c65b2447973285ebc5bc
kimjunseoo <zz0905k@naver.com>
2025-03-22 23:01:26 +0900

    Fix: 카카오 소셜 로그인

--- src/auth/dto/res/signInUserByKakao.res.dto.ts
+++ src/auth/dto/res/signInUserByKakao.res.dto.ts
@@ -1,6 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class signInUserByKakakoResDTO {
+  @ApiProperty({
+    description:
+      'type 값은 signin,signup 중 하나입니다. 로그인 시 signin, 회원가입 시 signup ',
+  })
+  type: string;
+
   @ApiProperty()
   accessToken: string;
 
