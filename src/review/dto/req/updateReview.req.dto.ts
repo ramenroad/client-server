4630519c6ac950f08876889d6a88ff16c3eb0448
Kim Junseo <zz0905k@naver.com>
2025-06-12 22:59:39 +0900

    🐛 Fix: 리뷰 수정 dto type

--- src/review/dto/req/updateReview.req.dto.ts
+++ src/review/dto/req/updateReview.req.dto.ts
@@ -25,7 +25,7 @@ export class updateReviewReqDTO {
   menus: string;
 
   @ApiProperty({
-    type: 'array',
+    type: 'string',
     description: '리뷰 이미지 urls / 쉼표로 구분해서 string으로 보내주세요',
     required: false,
   })
