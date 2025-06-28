787ac2e738bc86469f90eda266457a161f75635e
Kim Junseo <zz0905k@naver.com>
2025-06-29 01:30:46 +0900

    ✨ Feat : 네이버 소셜 로그인 API

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import {
   HttpStatus,
   Injectable,
   InternalServerErrorException,
+  NotAcceptableException,
   Res,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
@@ -15,6 +16,8 @@ import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { signInUserByKakakoResDTO } from './dto/res/signInUserByKakao.res.dto';
 import { JwtPayload, RtJwtPayload } from 'src/common/types/jwtpayloadtype';
+import { signInUserByNaverReqDTO } from './dto/req/signInUserByNaver.req.dto';
+import { signInUserByNaverResDTO } from './dto/res/signInUserByNaver.res.dto';
 
 @Injectable()
 export class AuthService {
@@ -244,5 +247,120 @@ export class AuthService {
     });
 
     return;
+  }
+
+
+  //작업 중
+  async signInUserByNaver(
+    dto: signInUserByNaverReqDTO,
+  ): Promise<signInUserByNaverResDTO> {
+    // <-> Naver API / 리프레쉬 토큰으로 네이버 토큰 재발급
+    const naverClientId = process.env.NAVER_DEVELOPERS_CLIENT_ID;
+    const naverClientSecret = process.env.NAVER_DEVELOPERS_CLIENT_SECRET;
+    
+    const header = {
+      "X-Naver-Client-Id": naverClientId,
+			"X-Naver-Client-Secret": naverClientSecret,
+    };
+
+    const apiUrl = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=' + naverClientId + '&client_secret=' + naverClientSecret + '&code=' + dto.authorizationCode + '&state=' + dto.state;
+
+    let response1;
+
+    try {
+      response1 = await axios.get(apiUrl, { headers: header });
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('네이버 토큰 발급 실패');
+    }
+
+    if (response1.status != 200){
+      throw new InternalServerErrorException(
+        '네이버 토큰 발급 실패(인증실패)',
+      );
+    }
+    
+    // <-> Naver API / 토큰으로 사용자 정보 받아오기 API 요청
+    const userToken = response1.data.access_token;
+
+    const header2 = {
+      Authorization: `Bearer ${userToken}`,
+    };
+
+    let response2;
+
+    try {
+      response2 = await axios.get(
+        `https://openapi.naver.com/v1/nid/me`,
+        {
+          headers: header2,
+        },
+      );
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('네이버 토큰 발급 실패');
+    }
+
+    if (response2.status != 200)
+      throw new InternalServerErrorException(
+        '네이버 사용자 정보 불러오기 실패',
+      );
+    
+    //사용자 정보가 DB에 존재하는지 확인(네이버 고유 id로 확인)
+    const user = await this.userModel.findOne({
+      naverId: String(response2.data.response.id),
+    });
+
+    //동일한 이메일로 가입된 유저가 있는지 확인
+    const existingUserByEmail = await this.userModel.findOne({
+      email: response2.data.response.email,
+      naverId: { $ne: String(response2.data.response.id) }, // 현재 네이버 ID가 아닌 다른 유저
+    });
+    
+    if (existingUserByEmail) {
+      throw new NotAcceptableException(`이메일 ${response2.data.response.email}은 이미 가입된 주소입니다.`);
+    }
+
+    if (!user) {
+      //존재하지 않는 경우, 유저 생성 후 토큰 발급
+      let newUser;
+      await this.userModel
+        .create({
+          email: response2.data.response.email,
+          naverId: String(response2.data.response.id),
+          nickname: await this.generateRandomNickname(),
+        })
+        .then((userInfo) => (newUser = userInfo));
+
+      const tokens = await this.getUserTokens(
+        newUser.id,
+        newUser.email,
+        newUser.nickname,
+      );
+
+      await this.updateUserRtHash(newUser.id, tokens.refreshToken);
+
+      const response = {
+        type: 'signup',
+        ...tokens,
+      };
+
+      return response;
+    } else {
+      //존재하는 경우, 로그인 토큰 발급
+      const tokens = await this.getUserTokens(
+        user.id,
+        user.email,
+        user.nickname,
+      );
+      await this.updateUserRtHash(user.id, tokens.refreshToken);
+
+      const response = {
+        type: 'signin',
+        ...tokens,
+      };
+
+      return response;
+    }
   }
 }
