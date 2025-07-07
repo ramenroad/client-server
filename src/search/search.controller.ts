9f0812de773c565b8a1ce1a28edf838313ad300a
kimjunseoo <zz0905k@naver.com>
2025-07-08 01:17:46 +0900

    ✨ Feat : 검색 API

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { SearchService } from './search.service';
+import { Public } from 'src/common/decorators/public.decorator';
 
 @Controller('search')
-export class SearchController {}
+export class SearchController {
+  constructor(private readonly searchService: SearchService) {}
+
+  @Public()
+  @Get('')
+  search(@Query('query') query: string) {
+    return this.searchService.search(query);
+  }
+}
--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SearchController } from './search.controller';
 import { SearchService } from './search.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ramenyaSchema } from 'schema/ramenya.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'ramenya', schema: ramenyaSchema }]),
+  ],
   controllers: [SearchController],
   providers: [SearchService],
 })
