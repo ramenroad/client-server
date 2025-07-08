a1961e0d216974389771cefe54dc024bf27bb0f0
kimjunseoo <zz0905k@naver.com>
2025-07-09 06:06:02 +0900

    💾 DB : 검색어 doc

--- schema/searchKeyword.schema.ts
+++ schema/searchKeyword.schema.ts
@@ -1,20 +1,41 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
 
 @Schema({
-  timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' },
+  timestamps: { createdAt: true, updatedAt: true },
   versionKey: false,
 })
 export class searchKeyword extends Document {
   @Prop({
     required: true,
+    enum: ['searchKeyword', 'ramenyaName'],
   })
-  type: string;
+  type: 'searchKeyword' | 'ramenyaName';
 
   @Prop({
     required: true,
   })
   keyword: string;
+
+  @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'user', required: true })
+  userId: MongooseSchema.Types.ObjectId;
+
+  @Prop({
+    type: MongooseSchema.Types.ObjectId,
+    ref: 'ramenya',
+    required: false,
+  })
+  ramenyaId?: MongooseSchema.Types.ObjectId;
+
+  @Prop({
+    default: 1,
+  })
+  count: number;
+
+  @Prop({
+    default: false,
+  })
+  isDeleted: boolean;
 }
 
 export const searchKeywordSchema = SchemaFactory.createForClass(searchKeyword);
